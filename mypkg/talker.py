e14c7da6cf3aa952902e0af9c1ab7968911413a4
hibiki703 <hibiki703@LAPTOP-5J6C32VG.localdomain>
2023-12-31 17:00:16 +0900

    merge

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from person_msgs.srv import Query
 
+<<<<<<< HEAD
 def cb(request, response):
     if request.name == "飯田響":
         response.age = 20 
@@ -13,4 +14,21 @@ def cb(request, response):
 rclpy.init()
 node = Node("talker")
 srv = node.create_service(Query, "query", cb)
+=======
+class Talker():
+    def __init__(self, nh):
+        self.pub = nh.create_publisher(Int16, "countup", 10)
+        self.n = 0
+        nh.create_timer(0.5, self.cb)
+
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
+
+rclpy.init()
+node = Node("talker")
+talker = Talker(node)
+>>>>>>> lesson10
 rclpy.spin(node)
