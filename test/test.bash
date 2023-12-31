4051033aac5b40300c1a85efe4016e690c3a49e7
hibiki703 <hibiki703@LAPTOP-5J6C32VG.localdomain>
2023-12-31 17:58:23 +0900

    fix

--- test/test.bash
+++ test/test.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
  dir=~
- [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
+ [ "$1" != "" ] && dir="$1" 
 
  cd $dir/ros2_ws
  colcon build
@@ -9,4 +9,4 @@
  timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
  cat /tmp/mypkg.log |
-	 grep 'Listen: 10'
+ grep 'Listen: 10'
